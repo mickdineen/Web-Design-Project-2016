663a2c269f78d4df0d206732bece36d03f6a5483
Michael Dineen <Michael Dineen>
2016-12-12 11:59:37 +0000

    website upload

---
+++ javas.js
@@ -0,0 +1,100 @@
+//price on decorations//
+function popOut1()
+		{
+		alert("80 euro");
+		}
+function popOut2()
+		{
+		alert("80 euro");	
+		}
+function popOut3()
+		{
+		alert("80 euro");	
+		}
+function popOut4()
+		{
+		alert("80 euro");	
+		}
+function popOut5()
+		{
+		alert("80 euro");	
+		}
+function popOut6()
+		{
+		alert("80 euro");	
+		}
+function popOut7()
+		{
+		alert("80 euro");	
+		}
+function popOut8()
+		{
+		alert("80 euro");	
+		}
+function popOut9()
+		{
+		alert("80 euro");	
+		}
+function popOut10()
+		{
+		alert("80 euro");	
+		}
+function popOut11()
+		{
+		alert("80 euro");	
+		}
+function popOut12()
+		{
+		alert("80 euro");	
+		}		
+	
+//price on trees//
+function popOuta()
+		{
+		alert("10 euro");	
+		}
+function popOutb()
+		{
+		alert("5 euro");	
+		}
+function popOutc()
+		{
+		alert("10 euro");	
+		}
+function popOutd()
+		{
+		alert("5 euro");	
+		}
+function popOute()
+		{
+		alert("10 euro");	
+		}
+function popOutf()
+		{
+		alert("5 euro");	
+		}
+function popOutg()
+		{
+		alert("10 euro");	
+		}
+function popOuth()
+		{
+		alert("5 euro");	
+		}
+function popOuti()
+		{
+		alert("10 euro");	
+		}
+function popOutj()
+		{
+		alert("5 euro");	
+		}
+function popOutk()
+		{
+		alert("10 euro");	
+		}
+function popOutl()
+		{
+		alert("5 euro");	
+		}		
+		
